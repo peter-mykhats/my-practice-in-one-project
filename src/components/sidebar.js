360c693c2e287d0a1f95c8c81e6c23e14460430b
Peter Mykhats <peter.mykhats@vkafe.org>
2021-01-12 17:17:11 +0200

    add component sidebar2

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,30 +1,19 @@
 import React from "react"
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 
 export default () => (
   <div>
     <ul>
       <li>
-        <Link to="/">Home</Link>
+        sidebar
+        {/* <Link to="/">Home</Link> */}
       </li>
-      <li>
-        <Link to="/my_components/component_1">component 1</Link>
-      </li>
-      <li>
-        <Link to="/my_components/component_2">component 2</Link>
-      </li>
-      <li>
-        <Link to="/">Part #1</Link>
-      </li>
-      <li>
-        <Link to="/">Part #2</Link>
-      </li>
-      <li>
-        <Link to="/">Part #3</Link>
-      </li>
-      <li>
-        <Link to="/">Part #4</Link>
-      </li>
+      <li>{/* <Link to="/my_components/component_1">component 1</Link> */}</li>
+      <li>{/* <Link to="/my_components/component_2">component 2</Link> */}</li>
+      <li>{/* <Link to="/">Part #1</Link> */}</li>
+      <li>{/* <Link to="/">Part #2</Link> */}</li>
+      <li>{/* <Link to="/">Part #3</Link> */}</li>
+      <li>{/* <Link to="/">Part #4</Link> */}</li>
     </ul>
   </div>
 )
