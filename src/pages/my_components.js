407de88bcf195776a581a32aae7d98b7dcc9bf43
Peter Mykhats <peter.mykhats@vkafe.org>
2021-01-12 17:11:14 +0200

    add component sidebar

--- src/pages/my_components.js
+++ src/pages/my_components.js
@@ -1,13 +1,13 @@
 import React from "react"
 import Layout from "../components/layout"
-// import Sidebar from "../components/Sidebar"
+import Sidebar from "../components/Sidebar"
 import "../scss/main.scss"
 
 export default () => (
   <Layout>
     <div className="my-component container">
       <h1>My component</h1>
-      {/* <Sidebar /> */}
+      <Sidebar />
     </div>
   </Layout>
 )
