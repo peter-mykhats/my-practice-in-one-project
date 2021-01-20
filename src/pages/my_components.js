39714e5aabf1e206905055ae235ebd30e87f4ef8
Peter Mykhats <peter.mykhats@vkafe.org>
2021-01-20 23:27:13 +0200

    fixes my_component

--- src/pages/my_components.js
+++ src/pages/my_components.js
@@ -3,11 +3,13 @@ import Layout from "../components/layout"
 import Sidebar from "../components/Sidebar"
 import "../scss/main.scss"
 
-export default () => (
-  <Layout>
-    <div className="my-component container">
-      <h1>My component</h1>
-      <Sidebar />
-    </div>
-  </Layout>
-)
+export default function My_component() {
+  return (
+    <Layout>
+      <div className="my-component container">
+        <h1>My component</h1>
+        <Sidebar />
+      </div>
+    </Layout>
+  )
+}
