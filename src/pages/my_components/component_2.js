289ef10a19c930ff88dcbb12954f5aa4139632f1
Peter Mykhats <peter.mykhats@vkafe.org>
2021-01-20 22:28:12 +0200

    come back sidebar

--- src/pages/my_components/component_2.js
+++ src/pages/my_components/component_2.js
@@ -1,9 +1,13 @@
 import React from "react"
 
 import Layout from "../../components/layout"
+import Sidebar from "../../components/Sidebar"
 
 export default () => (
   <Layout>
-    <h1>My component_2</h1>
+    <div className="my-component container">
+      <h1>My component_2</h1>
+      <Sidebar />
+    </div>
   </Layout>
 )
