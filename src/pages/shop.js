8b79d2a902a941478db3159574198f636f7d6b21
Peter Mykhats <peter.mykhats@vkafe.org>
2021-01-21 19:32:30 +0200

    fixes bugs

--- src/pages/shop.js
+++ src/pages/shop.js
@@ -1,9 +1,12 @@
 import React from "react"
-
 import Layout from "../components/layout"
 
-export default () => (
-  <Layout>
-    <h1>Shop page</h1>
-  </Layout>
-)
+export default function MyShop() {
+  return (
+    <Layout>
+      <div className={"myshop container"}>
+        <h1>Shop page</h1>
+      </div>
+    </Layout>
+  )
+}
