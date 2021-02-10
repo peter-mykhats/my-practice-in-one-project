856af19e1c8d5f61020c9bd6842021bbfd9bb081
Peter Mykhats <peter.mykhats@vkafe.org>
2021-02-10 10:28:28 +0200

    bug fixes2

--- src/pages/i_am.js
+++ src/pages/i_am.js
@@ -1,11 +1,10 @@
 import React from "react"
-
 import Layout from "../components/layout"
-import Star from "../components/Star"
+// import Star from "../components/Star"
 
 export default () => (
   <Layout>
     <h1>I am page</h1>
-    <Star />
+    {/* <Star /> */}
   </Layout>
 )
--- src/pages/shop.js
+++ src/pages/shop.js
@@ -2,7 +2,7 @@ import React from "react"
 import Item from "../components/Item"
 import Layout from "../components/layout"
 import { graphql } from "gatsby"
-import Sidebar from "../components/Sidebar"
+// import Sidebar from "../components/Sidebar"
 
 const MyShop = ({ data }) => {
   const allProducts = data.allMarkdownRemark.edges
@@ -10,7 +10,7 @@ const MyShop = ({ data }) => {
     <Layout>
       <div className={"myshop-block"}>
         <div className={"sidebar-block"}>
-          <Sidebar />
+          {/* <Sidebar /> */}
         </div>
         <div className={"myshop container"}>
           <h1>Shop page</h1>
